c7e27f3efc55715b0cb45387f8488dd6322e2712
DEVerissimocoder <danielverissimo1d@gmail.com>
2025-04-14 16:33:30 -0300

    exer 11- resolução usando função, lista e dicionário

--- exer11.py
+++ exer11.py
@@ -1,20 +1,19 @@
-dicion={}
-numeros=[]
-def numeros_inteiros(dicionario):
-    print(dicionario)
+
+lista=[]
+def numeros_inteiros(lista):    
+    dicionario={"produto": lista[0]*2*lista[1]/2,
+                "soma": (lista[0]*3)+lista[2],
+                "potencia":lista[2]**3
+                }
+    return dicionario
 
 opcao=1
 while opcao!=0:
+    numero=float(input("numero "))
+    lista.append(numero)    
     opcao = int(input("""
                 qualquer numero - continuar informando numeros
                 0- parar de informar números
                 """))
-    if (opcao==0):
-        break
-    numero=float(input("numero "))
-    numeros.append(numero)
-    if "numero" in dicion:
-        dicion["numero"] += numero
-    else:
-        dicion["numero"] = numero
-print(numeros_inteiros(dicion))
+    
+print(numeros_inteiros(lista))
